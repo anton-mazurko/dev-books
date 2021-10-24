90c0bf9c56809a6a03b9a470d2702045eb820912
Anton Mazurko <anton.mazurko@gmail.com>
2021-10-24 22:19:53 +0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bs-app',
+  template: `
+  <h1>{{pageTitle}}</h1>
+  <div>App component Test</div>
+  <bs-welcome></bs-welcome>
+  `
+})
+export class AppComponent {
+  pageTitle: string = "Dev Book Store"
+}
