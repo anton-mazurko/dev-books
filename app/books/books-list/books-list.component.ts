77721aa1a27931ee779835c870e130f31e2e3566
Anton Mazurko <anton.mazurko@gmail.com>
2021-10-30 13:40:02 +0200

    Event binding

--- app/books/books-list/books-list.component.ts
+++ app/books/books-list/books-list.component.ts
@@ -1,18 +1,23 @@
 import {Component} from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'bs-books-list',
-    templateUrl: 'books-list.component.html'
+  moduleId: module.id,
+  selector: 'bs-books-list',
+  templateUrl: 'books-list.component.html'
 })
 
 export class BooksListComponent {
-    imageWidth: number = 100;
+  imageWidth: number = 100;
+  showImage: boolean = true;
 
-    bookAuthor: string = "Tom Jones";
-    bookTitle: string = "War and Peace2";
-    bookPrice: string = "$29.95";
-    bookDescription: string = "Book of historical fiction";
-    bookReviews: number = 15;
-    bookImageUrl: string = "app/assets/images/656.jpg";
+  bookAuthor: string = "Tom Jones";
+  bookTitle: string = "War and Peace2";
+  bookPrice: string = "$29.95";
+  bookDescription: string = "Book of historical fiction";
+  bookReviews: number = 15;
+  bookImageUrl: string = "app/assets/images/656.jpg";
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
