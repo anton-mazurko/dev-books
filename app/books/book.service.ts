691c9024491ae334b0980e5a60ee0c8cc92526a9
Anton Mazurko <anton.mazurko@gmail.com>
2021-10-31 19:39:48 +0100

    Routing

--- app/books/book.service.ts
+++ app/books/book.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import { BooksListComponent } from './books-list/books-list.component';
 
 @Injectable()
 export class BookService {
@@ -16,8 +17,14 @@ export class BookService {
     return this._http
       .get('api/books/books.json')
       .map((response: Response) => <IBook[]> response.json())
-      .do(data => console.log(data))
+      // .do(data => console.log(data))
       .catch(this.handleError);
+  }
+
+  getBook(id: string): Observable<IBook> {
+    return this.getBooks()
+    .map((books: IBook[]) => books.find(book => book.id === id))
+    .do(data => console.log(data))
   }
 
   private handleError(error: Response) {
--- app/welcome/welcome.component.ts
+++ app/welcome/welcome.component.ts
@@ -2,11 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     moduleId: module.id,
-    selector: 'bs-welcome',
     templateUrl: 'welcome.component.html',
-    styleUrls: ['welcome.component.css']
 })
 
 export class WelcomeComponent {
-    pageTitle: string = "Welcome";
+
 }
